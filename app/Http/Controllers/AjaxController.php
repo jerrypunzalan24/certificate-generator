bf6623be2f541ed10954f2f9b3f8c945157a64ec
jerrypunzalan24 <jerrypunzalan24@gmail.com>
2019-05-03 19:19:41 +0800

    asdasd

--- app/Http/Controllers/AjaxController.php
+++ app/Http/Controllers/AjaxController.php
@@ -20,22 +20,22 @@ class AjaxController extends Controller
 			$mi = strtoupper((preg_match( "/\.$/",$request->input('mname'))) ? $request->input('mname')  : "{$request->input('mname')}.");
 			$school= $this->ucwords_new(explode(' ',$request->input('school')));
 			$currentDay = date("Y-m-d H:i:s");
-			// \DB::Insert("INSERT into data(first_name, middle_initial, last_name,school,date_created, email, gender,role)VALUES('{$request->input('fname')}','{$mi}','{$request->input('lname')}', '{$school}','{$currentDay}', '{$request->input('email')}', {$request->input('gender')}, '{$request->input('role')}')
-			// 	");
-			try{
-			\DB::table('data')->insert([
-				'first_name' => $request->input('fname'),
-				'middle_initial' => $mi,
-				'last_name' => $request->input('lname'),
-				'school' => $school,
-				'date' => $currentDay,
-				'email' => $request->input('email'),
-				'gender' => $request->input('gender'),
-				'role' => $request->input('role')
-			]);
-			}catch(Exception $e){
-				echo $e;
-			}
+			// // \DB::Insert("INSERT into data(first_name, middle_initial, last_name,school,date_created, email, gender,role)VALUES('{$request->input('fname')}','{$mi}','{$request->input('lname')}', '{$school}','{$currentDay}', '{$request->input('email')}', {$request->input('gender')}, '{$request->input('role')}')
+			// // 	");
+			// try{
+			// \DB::table('data')->insert([
+			// 	'first_name' => $request->input('fname'),
+			// 	'middle_initial' => $mi,
+			// 	'last_name' => $request->input('lname'),
+			// 	'school' => $school,
+			// 	'date' => $currentDay,
+			// 	'email' => $request->input('email'),
+			// 	'gender' => $request->input('gender'),
+			// 	'role' => $request->input('role')
+			// ]);
+			// }catch(Exception $e){
+			// 	echo $e;
+			// }
 		}
 	}
 
